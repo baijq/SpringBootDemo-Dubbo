712ac40eed5db71fb8fdae5a618b389944e1e6ac
baijinqiang <Baijqmail@163.com>
2019-05-09 16:20:31 +0800

    dubbo demo

---
+++ springboot-dubbo/dubbo-api/src/main/java/com/dubbo/service/HelloService.java
@@ -0,0 +1,7 @@
+package com.dubbo.service;
+
+public interface HelloService {
+
+    public String sayHello(String name);
+
+}
---
+++ springboot-dubbo/dubbo-consumer/src/main/java/com/dubbo/consumer/DubboConsumerApplication.java
@@ -0,0 +1,15 @@
+package com.dubbo.consumer;
+
+import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableDubboConfiguration
+@SpringBootApplication
+public class DubboConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboConsumerApplication.class, args);
+    }
+
+}
