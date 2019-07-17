7ba7387c05cca88c486440af053cb4676cf13c4d
baijinqiang <Baijqmail@163.com>
2019-07-17 13:22:16 +0800

    少量修改

--- springboot-demo/src/main/java/com/springboot/demo/DemoApplication.java
+++ springboot-demo/src/main/java/com/springboot/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+        System.out.println("http://127.0.0.1:8888/springboot/swagger-ui.html");
     }
 
 }
